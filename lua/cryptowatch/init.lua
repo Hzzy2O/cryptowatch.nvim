a2455a5ceba1c8845dfe2975869f452f2b768a7a
houzheng <capiiii@outlook.com>
2023-08-09 14:08:03 +0800

    num deal

--- lua/cryptowatch/init.lua
+++ lua/cryptowatch/init.lua
@@ -5,12 +5,14 @@ local default_config = {
   color = { bg = '#3D4E81', fg = '#A9B1D6', gui = 'bold' },
 }
 
+local prices = {}
+
 local coins = {
   { coin = 'BTC', color = { bg = '#3D4E81', fg = '#A9B1D6', gui = 'bold' } },
   { coin = 'ETH', color = { bg = '#5753C9', fg = '#d6d8df', gui = 'bold' } },
   { coin = 'LTC', color = { bg = '#6E7FF3', fg = '#e2e4e9', gui = 'bold' } },
 }
-local poll_interval = 1000
+local poll_interval = 10000
 
 local timer
 
@@ -20,23 +22,11 @@ local function get_coin_price(coin_pair)
     args = { "-s", "https://api.binance.com/api/v3/ticker/price?symbol=" .. coin_pair.coin .. coin_pair.pair },
     on_exit = function(j, return_val)
       local function format_price(price)
-        -- Convert the price to a number
         local price_num = tonumber(price)
 
-        if price_num < 0.01 then
-          -- Convert the price to a string
-          local price_str = tostring(price_num)
-
-          -- Find the index of the third non-zero digit after the decimal point
-          local index = 3
-          while price_str:sub(index + 1, index + 1) == "0" do
-            index = index + 1
-          end
-
-          -- Take the substring up to the third non-zero digit after the decimal point
-          return price_str:sub(1, index + 3)
+        if price_num < 1 then
+          return tostring(price)
         else
-          -- If the price is greater than 0, format it with 2 decimal places
           return string.format("%.2f", price_num)
         end
       end
@@ -46,7 +36,7 @@ local function get_coin_price(coin_pair)
           coin_price = format_price(coin_price)
           coin_price = string.format("%s %s", coin_pair.coin, coin_price)
           local coin_name = string.lower(coin_pair.coin)
-          vim.g[coin_name .. "_price"] = coin_price
+          prices[coin_name .. "_price"] = coin_price
         end)
       else
         print("Failed to retrieve " .. coin_pair.coin .. " price")
@@ -82,7 +72,7 @@ function M.setup(opts)
   local price_list = {}
   for _, coin_pair in ipairs(coins) do
     table.insert(price_list, {
-      function() return vim.g[string.lower(coin_pair.coin) .. "_price"] end,
+      function() return prices[string.lower(coin_pair.coin) .. "_price"] or "nil" end,
       color = coin_pair.color
     })
   end
